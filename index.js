c05c3126e4fec1df991056b20c226a3e4b2ad8a5
Shaiyko <Dexprojectkk@gmail.com>
2024-08-31 22:08:30 +0700

    d

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "dexnovelmysql-dexprojectkk-b7a4.c.aivencloud.com",
   port: 18541,
   user: "avnadmin",
-  password: "",
+  password: "AVNS_myphwaxv8xB0cnwCKXg",
   database: "dexnovel",
 });
 
